0ea486fedc1230fc899b52f53316261720437e37
Aditya <panwaraditya75@gmail.com>
2024-04-12 22:01:06 +0530

    auth and protectRoute complete

--- MAAYA_BACKEND/server.js
+++ MAAYA_BACKEND/server.js
@@ -1,9 +1,11 @@
 // these are my package imports
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 // these are my file imports
 import authRouts from "./routes/auth.js";
+import messageRouts from "./routes/messages.js";
 import connectMongoDB from "./Database/connectMongoDB.js";
 
 dotenv.config();
@@ -14,7 +16,10 @@ const port = process.env.PORT;
 
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRouts);
+app.use("/api/messages", messageRouts);
 
 
 app.listen(port, () => {
